01855e61f5ecf3c12f8c962eba06ded3565e0419
DmitryShuvalov <dmitry_shuvalov@mail.ru>
2022-05-10 11:12:36 +0300

    Add tests/helpers create_fake_dataset

---
+++ tests/helpers/create_fake_dataset.py
@@ -0,0 +1,16 @@
+from turtle import shape
+import pandas as pd
+import numpy as np
+
+def create_fake_dataset() -> pd.DataFrame():
+    shape0=100
+    shape1_binary=10
+    shape1_int = 50
+    df_binary = pd.DataFrame(np.random.randint(0,2,size=(shape0, shape1_binary)), columns=range(1,1+shape1_binary))
+    df_int=pd.DataFrame(np.random.randint(0,3000,size=(shape0, shape1_int)), columns=range(1+shape1_binary,1+shape1_binary+shape1_int)) 
+    df=pd.concat([df_binary, df_int], axis=1)
+    df['Id']=range(shape0)
+    df['target'] = np.random.randint(1,10,size=shape0)
+    df.set_index('Id')
+    return df
+create_fake_dataset()
