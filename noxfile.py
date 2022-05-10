f6aa60e77733ff0b61cfc4314f7a038b50599a86
DmitryShuvalov <dmitry_shuvalov@mail.ru>
2022-05-10 23:41:11 +0300

    Add 'noxfile.py' to nox check

--- noxfile.py
+++ noxfile.py
@@ -1,43 +1,38 @@
 import nox
+from nox.sessions import Session
 
-@nox.session(reuse_venv=True, python='3.9')
-def black(session):
-    #session.install('black')
+
+@nox.session(reuse_venv=True, python="3.9")
+def black(session: Session) -> None:
+    # session.install('black')
     if session.posargs:
-        folders = session.posargs
+        pathes = session.posargs
     else:
-        folders = ['src', 'tests']
-    session.run('black',
-         *folders,
-         external=True)
+        pathes = ["src", "tests", "noxfile.py"]
+    session.run("black", *pathes, external=True)
 
-@nox.session(reuse_venv=True, python='3.9')
-def mypy(session):
-    #session.install('mypy')
+
+@nox.session(reuse_venv=True, python="3.9")
+def mypy(session: Session) -> None:
+    # session.install('mypy')
     if session.posargs:
-        folders = session.posargs
+        pathes = session.posargs
     else:
-        folders = ['src', 'tests']
-    session.run('mypy',
-         *folders,
-         external=True)
+        pathes = ["src", "tests", "noxfile.py"]
+    session.run("mypy", *pathes, external=True)
 
-@nox.session(reuse_venv=False, python='3.9')
-def tests(session):
-    #session.install('pytest')
-    session.run('pytest',
-         'tests',
-         external=True)
 
-@nox.session(reuse_venv=True, python='3.9')
-def flake8(session):
-    #session.install('flake8')
+@nox.session(reuse_venv=False, python="3.9")
+def tests(session: Session) -> None:
+    # session.install('pytest')
+    session.run("pytest", "tests", external=True)
+
+
+@nox.session(reuse_venv=True, python="3.9")
+def flake8(session: Session) -> None:
+    # session.install('flake8')
     if session.posargs:
-        folders = session.posargs
+        pathes = session.posargs
     else:
-        folders = ['src', 'tests']
-    session.run('flake8',
-            '--append-config',
-            'flake8.ini',
-            *folders,
-            external=True)
+        pathes = ["src", "tests", "noxfile.py"]
+    session.run("flake8", "--append-config", "flake8.ini", *pathes, external=True)
