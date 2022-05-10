2ab15fc916ac21f725ff8fafd80729105a9210ab
DmitryShuvalov <dmitry_shuvalov@mail.ru>
2022-05-10 12:22:57 +0300

    Add 2 test to test_with_fake_data

--- tests/helpers/create_fake_dataset.py
+++ tests/helpers/create_fake_dataset.py
@@ -1,16 +1,16 @@
+from ast import Str
 from turtle import shape
 import pandas as pd
 import numpy as np
 
-def create_fake_dataset() -> pd.DataFrame():
+def create_fake_dataset(target: str) -> pd.DataFrame():
     shape0=100
     shape1_binary=10
     shape1_int = 50
     df_binary = pd.DataFrame(np.random.randint(0,2,size=(shape0, shape1_binary)), columns=range(1,1+shape1_binary))
     df_int=pd.DataFrame(np.random.randint(0,3000,size=(shape0, shape1_int)), columns=range(1+shape1_binary,1+shape1_binary+shape1_int)) 
     df=pd.concat([df_binary, df_int], axis=1)
-    df['Id']=range(shape0)
-    df['target'] = np.random.randint(1,10,size=shape0)
-    df.set_index('Id')
+    df["Id"]=range(shape0)
+    df[target] = np.random.randint(1,10,size=shape0)
+    df.set_index("Id")
     return df
-create_fake_dataset()
--- tests/test_with_fake_data.py
+++ tests/test_with_fake_data.py
@@ -2,8 +2,11 @@ from forest_ml import __version__
 from forest_ml.train import train
 import pytest
 from click.testing import CliRunner
+import pandas as pd
+from .helpers.create_fake_dataset import create_fake_dataset
 
-''' Testing with fake data fith file system isolation'''
+''' Testing with fake data and file system isolation'''
+
 def test_version():
      assert __version__ == '0.1.0'
 
@@ -12,4 +15,39 @@ def runner() -> CliRunner:
     """Fixture providing click runner."""
     return CliRunner()
 
+def test_error_for_invalid_model_name(
+    runner: CliRunner
+) -> None:
+     """It fails when model_name not in ['KNN', 'RFC', 'DTC']."""
+     with runner.isolated_filesystem():
+          target="target"
+          create_fake_dataset(target=target).to_csv("temp.csv")
+          result = runner.invoke(
+               train,
+               [
+                    "--csv_path", "temp.csv",
+                    "--target", target,
+                    "--model_name", "Fail",
+                    "--save_model", False
+               ],
+          )
+          assert result.exit_code == 2
+          assert "Invalid value for '-MN' / '--model_name'" in result.output
 
+def test_success_for_valid_model_name(
+    runner: CliRunner
+) -> None:
+     """It fails when model_name not in ['KNN', 'RFC', 'DTC']."""
+     with runner.isolated_filesystem():
+          target="target"
+          create_fake_dataset(target=target).to_csv("temp.csv")
+          result = runner.invoke(
+               train,
+               [
+                    "--csv_path", "temp.csv",
+                    "--target", target,
+                    "--model_name", "KNN",
+                    "--save_model", False
+               ],
+          )
+          assert result.exit_code == 0
